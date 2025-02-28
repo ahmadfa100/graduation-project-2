6718772e7f4726f8b856d49b9456b41973c645e5
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-03-01 01:07:39 +0300

    offer details added
    make leave as jsx file
    create offer details and button with thier css and jsx files
    add some classes to app. css

---
+++ HomePage/src/buttons.jsx
@@ -0,0 +1,29 @@
+import "./layout/buttons.css";
+import { useState } from "react";
+import { FaPhoneAlt, FaComments } from "react-icons/fa";
+
+function Call() {
+  return (
+    <button className="custom-button">
+      <FaPhoneAlt /> Call
+    </button>
+  );
+}
+
+function Chat() {
+  return (
+    <button className="custom-button">
+      <FaComments /> Chat
+    </button>
+  );
+}
+
+function Like() {
+return(
+    <button className="custom-button">
+    <img src="./content images/heart_after.png" alt="like"  className="icon"/>
+</button>
+);
+}
+
+export { Call, Chat, Like };
---
+++ HomePage/src/layout/leafLine.jsx
@@ -0,0 +1,16 @@
+import "./leaf_style.css";
+
+const leaf_style= ()=>{
+    return (
+   <div>
+         <div class="divider">
+      <div class="divider-line"></div>
+      <div class="divider-icon">
+        <img src="https://www.farmers.gov/themes/farmers_update/assets/img/leaves.svg" alt="ورقة" />
+      </div>
+      <div class="divider-line"></div>
+    </div>
+   </div>
+    );
+}
+export default leaf_style;

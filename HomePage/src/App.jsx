ce647ce499bb1cf4aed1cc140a6cbcd616fd5553
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-03-01 01:07:39 +0300

    offer details added
    make leave as jsx file
    create offer details and button with thier css and jsx files
    add some classes to app. css

--- HomePage/src/App.jsx
+++ HomePage/src/App.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import HomePage from "./HomePage";
+import Offferdetail from "./offerDetail";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import"./App.css";
 
+const images=[
+  "./content images/tomatoes-growing-garden.jpg",
+    "./Lands/Zitonjpg.jpg",
+    "./Lands/Map.jpg",
+]
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+     <Header></Header>
+      <Offferdetail pictures={images} location="Irbid" size="2" RentalType="annul"  title="Distinctive agricultural land in west Irbid for rent" price="2000" description="This fertile agricultural land in west Irbid is an ideal choice for farming enthusiasts and professionals alike. Spanning over 2 dunums, the land features nutrient-rich soil perfect for growing a variety of crops, from vegetables to fruit trees. Its strategic location offers easy access to water resources and is well-connected to nearby markets and towns. The surrounding landscape ensures a peaceful environment, with ample sunlight and favorable weather conditions year-round. Whether you're looking to expand your agricultural business or start a new venture, this land provides excellent potential for high yields and sustainable farming."/>
+      <Footer></Footer>
+    </div>
+  )
+  ;
 }
 
 export default App;
---
+++ HomePage/src/offerDetail.jsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import "./offerdetail.css";
+import LeafLine from "./layout/leafLine";
+import * as Button from"./buttons";
+
+const cellStyle = {
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+    margin: "5px",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+    padding: "10px",
+    cursor: "pointer",
+    width: "10%",
+    height: "20%",
+    
+}
+function Details(props){
+    return(
+        <div className="slider-container details">
+<h1 >Offer information </h1>
+<div><h3>
+    {props.title}
+</h3></div>
+<h4 id="price">{props.price} JOD</h4>
+<table >
+      <tbody>
+        <tr style={{ backgroundColor: "#f5f5f5" }}>
+          <td style={cellStyle}><strong>Size</strong></td>
+          <td style={cellStyle}>{props.size} Dunums</td>
+        </tr>
+        <tr>
+          <td style={cellStyle}><strong>Rental Term</strong></td>
+          <td style={cellStyle}>{props.RentalType}</td>
+        </tr>
+        <tr style={{ backgroundColor: "#f5f5f5" }}>
+          <td style={cellStyle}><strong>Address</strong></td>
+          <td style={cellStyle}>{props.location}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div className="custom-element" >
+    <LeafLine></LeafLine>
+    </div>
+    <p>
+        {props.description}
+    </p>
+    <div className="custom-element" >
+        <LeafLine></LeafLine>
+    </div>
+    <div className="button-container">
+<Button.Call></Button.Call>
+<Button.Chat></Button.Chat>
+    <Button.Like></Button.Like>
+
+    </div>
+</div>
+
+    );
+  }
+  ///////////////////////////////////////////////
+const OfferDetails = (props) => {
+ 
+  const [mainImage, setMainImage] = useState("./Lands/Land_1.jpg");
+
+
+  const thumbnails = [
+    "./content images/tomatoes-growing-garden.jpg",
+    "./Lands/Zitonjpg.jpg",
+    "./Lands/Map.jpg",
+  ];
+
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+
+  const changeImage = (index) => {
+    setMainImage(thumbnails[index]);
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="page" >
+    <div className="slider-container">
+      <div className="main-slide">
+        <img id="mainImage" src={mainImage} alt="Main Slide" />
+      </div>
+      <div className="thumbnail-container">
+        {thumbnails.map((thumb, index) => (
+          <img
+            key={index}
+            src={thumb}
+            alt={`Thumb ${index + 1}`}
+            onClick={() => changeImage(index)}
+            className={activeIndex === index ? "active" : ""}
+          />
+        ))}
+      </div>
+
+     
+      <div className="pagination-dots">
+        {thumbnails.map((_, index) => (
+          <span
+            key={index}
+            onClick={() => changeImage(index)}
+            className={activeIndex === index ? "active" : ""}
+          ></span>
+        ))}
+      </div>
+ 
+      </div>
+      
+      <div className="slider-container">
+      {Details(props)}
+
+</div>
+    </div>
+  );
+};
+
+export default OfferDetails;

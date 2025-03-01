f8276ee9b20385d6fa01697f28c74d8117887da3
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-03-01 23:04:46 +0300

    chat page done

--- HomePage/src/App.jsx
+++ HomePage/src/App.jsx
@@ -4,15 +4,16 @@ import Offferdetail from "./offerDetail";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import AddOffer from "./addOffer";
+import Chat from "./chat";
 import"./App.css";
 
 
 function App() {
   return (
     <div>
-     <Header></Header>
-     <Offferdetail  location="Irbid" size="2" RentalType="annul"  title="Distinctive agricultural land in west Irbid for rent" price="2000" description="This fertile agricultural land in west Irbid is an ideal choice for farming enthusiasts and professionals alike. Spanning over 2 dunums, the land features nutrient-rich soil perfect for growing a variety of crops, from vegetables to fruit trees. Its strategic location offers easy access to water resources and is well-connected to nearby markets and towns. The surrounding landscape ensures a peaceful environment, with ample sunlight and favorable weather conditions year-round. Whether you're looking to expand your agricultural business or start a new venture, this land provides excellent potential for high yields and sustainable farming."/>
-      <Footer></Footer>
+     
+    <Chat chat_title="Distinctive agricultural land in west Irbid for rent" offer_image="./Lands/Map.jpg"></Chat>
+      
     </div>
   )
   ;

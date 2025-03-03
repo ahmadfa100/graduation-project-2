aeaa597bb559dded52f7d7e06af721b7910a1255
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-03-03 23:11:47 +0300

    some fixes

--- HomePage/src/App.jsx
+++ HomePage/src/App.jsx
@@ -1,99 +1,50 @@
-<<<<<<< HEAD
-=======
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import HomePage from "./Componenet/HomePage";
-// import Offferdetail from "./Componenet/offerDetail";
-// import Educational from "./Componenet/Educational";
-// import ArticleDetail from "./Componenet/ArticleDetail";
-// // import Chat from "./Componenet/chat";
-
-// import Header from "./layout/Header";
-// import Footer from "./layout/Footer";
-// import AddOffer from "./Componenet/addOffer";
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Header />
-
-//       <Routes>
-//         <Route path="/HomePage" element={<HomePage />} />
-//         <Route path="/offerDetail" element={<Offferdetail />} />
-//         <Route path="/Educational" element={<Educational />} />
-//         <Route
-//           path="/ArticleDetail/:id"
-//           element={<ArticleDetail />}
-//         />
-//         <Route path="/addOffer" element={<AddOffer />} />
-//       </Routes>
-
-//       {/* <div>
-//         <Chat
-//           chat_title="Distinctive agricultural land in west Irbid for rent"
-//           offer_image="./Lands/Map.jpg"
-//         ></Chat>
-//       </div> */}
-
-//       <Footer />
-//     </Router>
-//   );
-// }
-
-// export default App;
-
->>>>>>> 4e73dd7d398b29e108410e3a6a8fcc896ac00300
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Componenet/HomePage";
 import Offferdetail from "./Componenet/offerDetail";
 import Educational from "./Componenet/Educational";
 import ArticleDetail from "./Componenet/ArticleDetail";
-import AddOffer from "./Componenet/addOffer";
+// import Chat from "./Componenet/chat";
 
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
+import AddOffer from "./Componenet/addOffer";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Header />
-<<<<<<< HEAD
-      <AddOffer />
-      <Footer />
-    </Router>
-  );
-}
 
-export default App;
-
-/*
-=======
-
->>>>>>> 4e73dd7d398b29e108410e3a6a8fcc896ac00300
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/offer" element={<Offferdetail />} />
-        <Route path="/education" element={<Educational />} />
+      
+ <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/offer" element={<Offferdetail />} />
+       <Route path="/education" element={<Educational />} />
         <Route path="/article/:id" element={<ArticleDetail />} />
-        <Route path="/AddOffer" element={<AddOffer />} />
-      </Routes>
-<<<<<<< HEAD
-      
-      
-*/
-=======
+      <Route path="/AddOffer" element={<AddOffer />} />
+   </Routes>
+
+     
 
       <Footer />
     </Router>
   );
 }
->>>>>>> 4e73dd7d398b29e108410e3a6a8fcc896ac00300
+
+ export default App;
 
 
-{/* <Offferdetail title="green flat land in irbid" price="2000" RentalType="monthly"  size="2" location="irbid"
+      
+   /* <div>
+        <Chat
+          chat_title="Distinctive agricultural land in west Irbid for rent"
+          offer_image="./Lands/Map.jpg"
+        ></Chat>
+      </div> */   
+
+
+/* <Offferdetail title="green flat land in irbid" price="2000" RentalType="monthly"  size="2" location="irbid"
 description="Discover this expansive and serene green 
 flat land located in the heart of Irbid, perfect for agricultural 
 projects, recreational spaces, or future development. Spanning 2 dunums, this property offers
@@ -104,4 +55,4 @@ projects, recreational spaces, or future development. Spanning 2 dunums, this pr
    a viewing and explore the potential of this beautiful property!"
 
 
-></Offferdetail> */}
+></Offferdetail> */

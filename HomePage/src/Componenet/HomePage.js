accd2c7c36c246f308bf391e25da623cd0ba3878
ahmadfa100 <ahmad100.fa100@gmail.com>
2025-03-03 20:05:13 +0300

    some changes and first try to link all pages

--- HomePage/src/Componenet/HomePage.js
+++ HomePage/src/Componenet/HomePage.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import '../style/HomePage.css'; 
+import React, { useState } from "react";
+import "../style/HomePage.css";
 
-import '../layout/Header.css';  
-import '../layout/Footer.css';  
-import Header from '../layout/Header';  
-import Divider from '../layout/leaf';
-import Footer from '../layout/Footer';  
+import "../layout/Header.css";
+import "../layout/Footer.css";
+import Header from "../layout/Header";
+import Divider from "../layout/leaf";
+import Footer from "../layout/Footer";
 
-import { offers } from './HomePage_data';
+import { offers } from "./HomePage_data";
 
-import Season from './Season';
-import OffersSection from './OffersSection ';
+import Season from "./Season";
+import OffersSection from "./OffersSection ";
 
-import { FaPhone, FaComments, FaHeart, FaRegHeart } from 'react-icons/fa';
+import { FaPhone, FaComments, FaHeart, FaRegHeart } from "react-icons/fa";
 function HomePage() {
   const [favoriteOffers, setFavoriteOffers] = useState([]);
 
@@ -26,11 +26,10 @@ function HomePage() {
 
   return (
     <div className="home-page">
-
       <div className="search-container">
-        <input 
-          type="text" 
-          placeholder="Search for lands..." 
+        <input
+          type="text"
+          placeholder="Search for lands..."
           className="search-input"
         />
         <button className="search-button">Search</button>
@@ -41,40 +40,38 @@ function HomePage() {
       {/* Action Cards */}
       <div className="action-container">
         <div className="action-card">
-          <img 
-            src="https://i.pinimg.com/736x/b9/7b/62/b97b62328e8f52113257b9ca1bd698e5.jpg" 
-            alt="Rent Land" 
+          <img
+            src="https://i.pinimg.com/736x/b9/7b/62/b97b62328e8f52113257b9ca1bd698e5.jpg"
+            alt="Rent Land"
             className="action-image"
           />
           <div className="action-content">
             <h2>Rent Land</h2>
-            <button className="action-button">Learn More</button>
+            <button className="action-button card_home">Learn More</button>
           </div>
         </div>
 
         <div className="action-card">
-          <img 
-            src="https://i.pinimg.com/736x/97/e9/aa/97e9aab718aee4e3b912bbcf86b41ef1.jpg" 
-            alt="Offer Land" 
+          <img
+            src="https://i.pinimg.com/736x/97/e9/aa/97e9aab718aee4e3b912bbcf86b41ef1.jpg"
+            alt="Offer Land"
             className="action-image"
           />
           <div className="action-content">
             <h2>Offer Land</h2>
-            <button className="action-button">Offer Land</button>
+            <button className="action-button card_home">Offer Land</button>
           </div>
         </div>
       </div>
-          
+
       <Divider />
 
-
       {/* OffersSection */}
-      <OffersSection 
+      <OffersSection
         offers={offers}
         favoriteOffers={favoriteOffers}
         toggleFavorite={toggleFavorite}
       />
-
     </div>
   );
 }

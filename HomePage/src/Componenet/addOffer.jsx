d8d85b624d8424600f186ac1536343ff87181eac
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-03-04 23:31:41 +0300

    addOffer fix
    fix numbering  , improve readability

--- HomePage/src/Componenet/addOffer.jsx
+++ HomePage/src/Componenet/addOffer.jsx
@@ -1,53 +1,78 @@
-import"../style/addOffer.css";
-function unit_input(type,unit,message,name){
-    return(
-        <div className="unit-Input">
-               <span className="unit">{unit}</span>
-               <input className="textInput composite" type={type} placeholder={message} name={name} required />
-               </div>
-    );
+import "../style/addOffer.css";
+
+function UnitInput(props) {
+  return (
+    <div className="unit-Input">
+      <span className="unit">{props.unit}</span>
+      <input
+        className="textInput composite"
+        type={props.type}
+        placeholder={props.message}
+        name={props.name}
+        max={props.max}
+        min={props.min}
+        required
+      />
+    </div>
+  );
 }
 
-function Input(props){
- 
-    return( <input className="textInput" type={props.type} placeholder={props.message} name={props.name} required />);  
+function Input(props) {
+  return (
+    <input
+      className="textInput"
+      type={props.type}
+      placeholder={props.message}
+      name={props.name}
+      required
+      min={props.min}
+      max={props.max}
+    />
+  );
 }
 
-function AddOffer(props){
+function AddOffer(props) {
+  return (
+    <div className="page">
+      <div className="add_offer">
+        <h3>land lease information</h3>
+        <form>
+          <div className="group-input">
+            <Input type="text" message="Enter offer title" name="offer_title" />
+            <UnitInput type="text" unit="m²" message="Enter the number of dunums" name="size" />
+            {/* <UnitInput type="number" unit="Years" message="Enter Rental Duration" name="duration" /> */}
+            <div className="detailed-input">
+              <Input type="number" message="Lease duration(years)" name="years" min="0" max="100" />
+              <Input type="number" message="Lease duration(months)" name="months" min="0" max="12"/>
+            </div>
+            <UnitInput type="text" unit="JOD" message="Enter price" name="price" />
+            <Input
+              type="text"
+              message="Enter offer location (e.g., Amman Sweileh)"
+              name="location"
+            />
+            <textarea
+              className="textInput"
+              placeholder="Enter a detailed description of the land and the offer"
+              rows="5"
+              required
+            ></textarea>
 
-    return (
-        <div className="page">
-            <div className="add_offer">
-                <h3>land lease information</h3>
-            <form>
-               <div className="group-input">
-         <Input type="text" message="Enter offer title"  name="offer_title"/>
-               {unit_input("number","Dunums","Enter the number of dunums","size")}
-               {//unit_input("number","Years","Enter Rental Duration","duration")
-               }
-               <div className="detailed-input">
+            <div className="center">
+              <label className="file-upload">
+                Click here to upload images (you can select multiple images)
+                <input type="file" multiple hidden name="images" />
+              </label>
 
-               <Input type="number" message="Lease duration(years)"  name="years"/>
-               <Input type="number" message="Lease duration(months)"  name="months"/>
-               </div>
-               {unit_input("text","JOD","Enter price","price")}
-               <Input type="text" message="Enter offer location (e.g., Amman Sweileh)"  name="location"/>
-               <textarea className="textInput" placeholder="Enter a detailed description of the land and the offer" rows="5" required></textarea>
-               
-      <div className="center">
-      <label class="file-upload">
-            Click here to upload images (you can select multiple images)
-            <input type="file" multiple hidden  name="images"/>
-        </label>
-
-        <button type="submit" className="submit">Save and Publish</button>
-
-
+              <button type="submit" className="submit">
+                Save and Publish
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-               </div>
-               </form>
-               </div>
-        </div>
-    );
+    </div>
+  );
 }
+
 export default AddOffer;

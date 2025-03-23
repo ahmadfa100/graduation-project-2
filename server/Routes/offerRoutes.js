9e176465f5661a28d2d528e332ded2fe0741195d
ahmadfa100 <ahmad100.fa100@gmail.com>
2025-03-23 16:05:57 +0300

    edit the route

--- server/Routes/offerRoutes.js
+++ server/Routes/offerRoutes.js
@@ -1,14 +1,8 @@
 import express from "express";
-import multer from "multer";
-import { getOffer, addOffer, updateOffer, deleteOffer } from "../Controllers/offer.js";
+import { getAllOffers } from "../Controllers/filterOffers.js"; 
 
 const router = express.Router();
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
 
-router.get("/getOffer/:offerID", getOffer);
-router.post("/addOffer", upload.array("images", 10), addOffer);
-router.put("/updateOffer/:offerID", upload.array("images", 10), updateOffer);
-router.delete("/deleteOffer/:offerID", deleteOffer);
+router.get("/", getAllOffers);
 
 export default router;

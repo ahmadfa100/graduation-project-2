833fc22dacfcd23748cbd8efd903495b83234430
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-03-25 00:37:01 +0300

    chat part2

--- server/index.js
+++ server/index.js
@@ -17,10 +17,41 @@ app.use("/", offersRoutes);
 app.use("/", router);
 app.use("/api/notifications", notificationsRoutes);
 
+//////////////////////////////////////////
 
+import { Server } from "socket.io";
+import { createServer } from "http";
 
+const server = createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("New client connected :", socket.id);
+
+  socket.on("join",(data)=>{
+    socket.join(data);
+    console.log("User joined room :", data);
+  })
+
+  socket.on("sendMessage", (data) =>{
+    console.log("Sent message :", data.message);
+
+    socket.broadcast.emit("RecivedMessage", data.message);
+    //socket.to(data.room).emit("RecivedMessage",data);
+  });
+  socket.on("disconnect", () => {
+    console.log("Client disconnected :", socket.id);
+  });
+}
+
+);
+ 
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });

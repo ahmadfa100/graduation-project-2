f156b90cc153d2274bcc734a25d9c7c9c203e096
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-03-24 19:40:42 +0300

    chat backend part 1

--- server/index.js
+++ server/index.js
@@ -4,18 +4,22 @@ import env from "dotenv";
 import offersRoutes from "./Routes/offerRoutes.js";
 import router from "./Routes/filterRoutes.js";
 import notificationsRoutes from "./Routes/notificationsRoutes.js";
+//import chat from "./Routes/chat.js";
 
 env.config();
 const app = express();
 const port = process.env.PORT || 3001;
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/", offersRoutes);
+//app.use("/", chat);
 app.use("/", router);
 app.use("/api/notifications", notificationsRoutes);
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

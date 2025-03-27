3794bb3c90ebc6cc7b06dfd45f17e5f8fe1fb496
Ahmad Faisal <ahmad100.fa100@example.com>
2025-03-27 14:03:48 +0300

    Cobine both Ahmad code And Zaid code.

--- server/Routes/offerRoutes.js
+++ server/Routes/offerRoutes.js
@@ -4,10 +4,9 @@ import {
   getOffer, 
   addOffer, 
   updateOffer, 
-  deleteOffer 
+  deleteOffer, 
+  getAllOffers 
 } from "../Controllers/offer.js";
-
-import { getAllOffers } from "../Controllers/filterOffers.js"; 
 
 const router = express.Router();
 const storage = multer.memoryStorage();
@@ -17,7 +16,6 @@ router.get("/getOffer/:offerID", getOffer);
 router.post("/addOffer", upload.array("images", 10), addOffer);
 router.put("/updateOffer/:offerID", upload.array("images", 10), updateOffer);
 router.delete("/deleteOffer/:offerID", deleteOffer);
-
 router.get("/offers", getAllOffers);
 
 export default router;

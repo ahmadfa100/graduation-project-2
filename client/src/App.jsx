7eaad0193f52d965331a9eb8c0bb5e82bf76ba35
Ahmad Faisal <ahmad100.fa100@gmail.com>
2025-04-23 07:31:00 +0300

    Farmer Dashboard is done.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,14 +14,16 @@ import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import AddOffer from "./Componenet/addOffer";
 import UpdateOffer from "./Componenet/updateOffer";
-import "./App.css";
 import AccountInf from "./Componenet/AccountInf";
 import DashBoard from "./Componenet/DashBoard";
 import OfferDetails from "./Componenet/offerDetail";
+import FarmerDashboard from "./Componenet/Farmer_Dashboard";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">   {}
+    <div className="App">
       <Router>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
@@ -34,10 +36,8 @@ function App() {
                 <div className="content">
                   <Routes>
                     <Route path="/" element={<HomePage />} />
-                    <Route
-                      path="/OfferDetails/:offerID"
-                      element={<OfferDetails />}
-                    />
+                    <Route path="/homepage" element={<HomePage />} />
+                    <Route path="/OfferDetails/:offerID" element={<OfferDetails />} />
                     <Route path="/education" element={<Educational />} />
                     <Route path="/article/:id" element={<ArticleDetail />} />
                     <Route
@@ -48,25 +48,30 @@ function App() {
                         </NotificationsProvider>
                       }
                     />
- <Route
-   path="/updateOffer/:offerID"
-   element={
-     <NotificationsProvider>
-       <UpdateOffer />
-     </NotificationsProvider>
-   }
- />
-                    <Route path="/about" element={<About />} />
                     <Route
-                      path="/chat/:offerID/:ReceiverID"
-                      element={<Chat/>}
+                      path="/updateOffer/:offerID"
+                      element={
+                        <NotificationsProvider>
+                          <UpdateOffer />
+                        </NotificationsProvider>
+                      }
                     />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/chat/:offerID/:ReceiverID" element={<Chat />} />
                     <Route path="/AccountInf" element={<AccountInf />} />
                     <Route
                       path="/DashBoard"
                       element={
                         <NotificationsProvider>
                           <DashBoard />
+                        </NotificationsProvider>
+                      }
+                    />
+                    <Route
+                      path="/FarmerDashboard"
+                      element={
+                        <NotificationsProvider>
+                          <FarmerDashboard />
                         </NotificationsProvider>
                       }
                     />
